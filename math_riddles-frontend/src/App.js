3a313b4339914660e24163fcd4210335924e9422
EvilPanda <joratractoristul@gmail.com>
2023-04-26 22:14:50 +0300

    Make the necessary improvements in App.js

--- math_riddles-frontend/src/App.js
+++ math_riddles-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getAllJokes } from './api/jokeAPI';
+import { getAllJokes,getJokeById } from './api/jokeAPI';
 import Settings from './components/Settings';
 import Joke from './components/Joke';
 import './App.css';
@@ -7,26 +7,70 @@ import './App.css';
 const App = () => {
   const [joke, setJoke] = useState(null);
   const [currentJokeIndex, setCurrentJokeIndex] = useState(0);
-  const [settings, setSettings] = useState({
-    operation: 'addition',
-    numberRange: [1, 100],
-  });
+  // const [settings, setSettings] = useState({
+  //   operation: 'addition',
+  //   numberRange: [1, 100],
+  // });
   const [isFirstJoke, setIsFirstJoke] = useState(false);
   const [isLastJoke, setIsLastJoke] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     fetchJoke();
-  }, [settings]);
+  }, []);
 
   useEffect(() => {
+    // if (currentJokeIndex == 1) {
+    //   setIsFirstJoke(true);
+    // } else {
+    //   setIsFirstJoke(false);
+    // }
     if (joke && joke.length > 0){
       setIsFirstJoke(currentJokeIndex === 0);
      setIsLastJoke(currentJokeIndex === joke.length - 1);
     }
-  }, [currentJokeIndex]);
+  }, [currentJokeIndex, joke]);
   
+  useEffect(() => {
+    if(error) {
+      setIsLastJoke(true);
+    }
+  }, [error])
+
   
+  const handleNextJoke = () => {
+    // const newIndex = currentJokeIndex + 1;
+    // setCurrentJokeIndex(newIndex);
+    // fetchJoke();
+    const newIndex = currentJokeIndex + 1;
+    if (newIndex < joke.length) {
+      setCurrentJokeIndex(newIndex);
+    }
+  };
+  
+  const handlePreviousJoke = () => {
+    // const newIndex = currentJokeIndex - 1;
+    // setCurrentJokeIndex(newIndex);
+    // fetchJoke();
+    const newIndex = currentJokeIndex - 1;
+    if (newIndex >= 0) {
+      setCurrentJokeIndex(newIndex);
+    }
+  };
+  // console.log(joke.length)
+
   const fetchJoke = async () => {
+    // try {
+    //   const jokes = await getJokeById(currentJokeIndex);
+    //   // const randomIndex = Math.floor(Math.random() * jokes.length);
+    //   setJoke(jokes);
+    //   // console.log(jokes)
+    //   // console.log(jokes)
+    //   // setCurrentJokeIndex(0); // reset currentJokeIndex to 0 when new jokes are fetched
+    // } catch (error) {
+    //   setError(error);
+    //   console.error('Error fetching jokes:', error);
+    // }
     try {
       const jokes = await getAllJokes();
       const randomIndex = Math.floor(Math.random() * jokes.length);
@@ -38,26 +82,7 @@ const App = () => {
     }
   };
 
-  const handleSettingsChange = (newSettings) => {
-    setSettings(newSettings);
-  };
-
-
-
-  const handleNextJoke = () => {
-    const newIndex = currentJokeIndex + 1;
-    if (newIndex < joke.length) {
-      setCurrentJokeIndex(newIndex);
-    }
-  };
   
-  const handlePreviousJoke = () => {
-    const newIndex = currentJokeIndex - 1;
-    if (newIndex >= 0) {
-      setCurrentJokeIndex(newIndex);
-    }
-  };
-  // console.log(joke.length)
 
 
   return (
@@ -67,7 +92,7 @@ const App = () => {
     </header>
     <div className="app">
       {/* <Settings onChange={handleSettingsChange} /> */}
-      {joke && <Joke joke={joke[currentJokeIndex]} settings={settings} onNextJoke={handleNextJoke} onPreviousJoke={handlePreviousJoke} isFirstJoke ={isFirstJoke} isLastJoke={isLastJoke}/>
+      {joke && <Joke joke={joke[currentJokeIndex]} onNextJoke={handleNextJoke} onPreviousJoke={handlePreviousJoke} isFirstJoke ={isFirstJoke} isLastJoke={isLastJoke}/>
 }
     </div>
     </>
