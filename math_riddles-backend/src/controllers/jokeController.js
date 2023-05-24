12aad513d409811434b25cd1b9395d25a7ffd3b1
EvilPanda <joratractoristul@gmail.com>
2023-05-25 02:48:18 +0300

    Modify the jokeController for the addJoke endpoint

--- math_riddles-backend/src/controllers/jokeController.js
+++ math_riddles-backend/src/controllers/jokeController.js
@@ -33,13 +33,39 @@ exports.getJoke = async (req, res) => {
 
 // Add a new joke
 exports.addJoke = async (req, res) => {
-  const { question, encodedLetters } = req.body;
+  const { id, question, encodedLetters, difficulty } = req.body;
 
-  if (!question || !encodedLetters) {
-    return res.status(400).json({ message: 'Question and encoded letters are required' });
+  // Check for missing or empty fields
+  if (!id || id === "") {
+    return res.status(400).json({ message: 'ID is required and cannot be empty.' });
   }
 
-  const joke = new Joke({ question, encodedLetters });
+  if (!question || question === "") {
+    return res.status(400).json({ message: 'Question is required and cannot be empty.' });
+  }
+
+  if (!encodedLetters || encodedLetters.length === 0) {
+    return res.status(400).json({ message: 'Encoded letters are required and cannot be empty.' });
+  }
+
+  if (!difficulty || difficulty === "") {
+    return res.status(400).json({ message: 'Difficulty is required and cannot be empty.' });
+  }
+
+  // Check if the difficulty is valid
+  const validDifficulties = ["easy", "medium", "hard"];
+  if (!validDifficulties.includes(difficulty)) {
+    return res.status(400).json({ message: 'Difficulty must be one of the following: easy, medium, hard.' });
+  }
+
+  // Check if the ID already exists in the database
+  const existingJoke = await Joke.findOne({ id: id });
+  if (existingJoke) {
+    return res.status(400).json({ message: 'A joke with this ID already exists. Please use a unique ID.' });
+  }
+
+  // Create the new joke
+  const joke = new Joke({ id, question, encodedLetters, difficulty });
 
   try {
     const savedJoke = await joke.save();
